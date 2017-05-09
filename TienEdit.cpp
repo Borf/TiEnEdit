b6874b11957cb9e7dc5f2da9431c6ba797fec123
Johan Talboom <borfje@gmail.com>
2017-05-09 13:56:22 +0200

    Improved stubs

--- TienEdit.cpp
+++ TienEdit.cpp
@@ -78,31 +78,72 @@
 
 
 
-class Rotator : public vrlib::tien::Component
-{
-	virtual void update(float elapsedTime, vrlib::tien::Scene& scene)
-	{
-		node->transform->rotate(glm::vec3(0, elapsedTime, 0));
-	}
-	virtual json toJson(json & meshes) const override
-	{
-		json ret;
-		ret["type"] = "rotate";
-		return ret;
-	}
-};
-
-
 class StubComponent : public vrlib::tien::Component
 {
 public:
 	json stub;
+	json values;
+
+	StubComponent()
+	{
+
+	}
+	StubComponent(json v)
+	{
+		values = v;
+	}
+
 	virtual json toJson(json & meshes) const override
 	{
-		json ret;
-		ret["type"] = stub["type"];
+		json ret = values;
+		ret["type"] = stub["name"];
 		return ret;
 	}
+	virtual void buildEditor(vrlib::tien::EditorBuilder* builder, bool folded)
+	{
+		builder->addTitle(stub["name"].get<std::string>() + " (stub)");
+		if (folded)
+			return;
+
+		for (auto &property : stub["properties"])
+		{
+			if (values.find(property["name"]) == values.end())
+				values[property["name"].get<std::string>()] = property["default"];
+
+			builder->beginGroup(property["name"]);
+			if (property["type"] == "string")
+				builder->addTextBox(values[property["name"].get<std::string>()].get<std::string>(), [this, &property](const std::string &newValue) { values[property["name"].get<std::string>()] = newValue; });
+			else if (property["type"] == "float")
+				builder->addFloatBox(values[property["name"].get<std::string>()].get<float>(), 
+					property["min"].get<float>(), 
+					property["max"].get<float>(), [this, &property](float newValue) {values[property["name"].get<std::string>()] = newValue; });
+			else if (property["type"] == "bool")
+				builder->addCheckbox(values[property["name"].get<std::string>()].get<bool>(), [this, &property](bool newValue) {values[property["name"].get<std::string>()] = newValue; });
+			else if (property["type"] == "color")
+			{
+				const json &colorValue = values[property["name"].get<std::string>()];
+				builder->addColorBox(glm::vec4(colorValue[0], colorValue[1], colorValue[2], colorValue[3]), [this, &property](const glm::vec4 &newValue) {
+					for (int i = 0; i < 4; i++)
+						values[property["name"].get<std::string>()][i] = newValue[i];
+				});
+			}
+			else if (property["type"] == "enum")
+			{
+				std::vector<std::string> enumValues;
+				for (const std::string &v : property["values"])
+					enumValues.push_back(v);
+				builder->addComboBox(values[property["name"].get<std::string>()].get<std::string>(), enumValues, [this, &property](const std::string &newValue) {values[property["name"].get<std::string>()] = newValue; });
+			}
+			else
+				vrlib::logger << "Unknown stub type: " << property["type"] << Log::newline;
+
+			builder->endGroup();
+		}
+
+
+
+	}
+
 };
 
 
@@ -149,9 +190,6 @@ std::map<std::string, ComponentPair> componentFactory =
 	{ "TerrainCollider", ComponentPair(
 		[](vrlib::tien::Node* n) { return new vrlib::tien::components::TerrainCollider(n); },
 		[](const json &data, const json &totalJson) { return new vrlib::tien::components::TerrainCollider(); }) },
-	{ "rotator", ComponentPair(
-		[](vrlib::tien::Node* n) { return new Rotator(); },
-		[](const json &data, const json &totalJson) { return new vrlib::tien::components::ModelRenderer(data); }) },
 	{ "PostProcessing Bloom", ComponentPair(
 		[](vrlib::tien::Node* n) { return new vrlib::tien::components::postprocessors::Bloom(); },
 		[](const json &data, const json &totalJson) { return new vrlib::tien::components::postprocessors::Bloom(); }) },
@@ -210,7 +248,7 @@ void TienEdit::init()
 		},
 		[stub](const json &data, const json &totalJson)
 		{
-			StubComponent* c = new StubComponent();
+			StubComponent* c = new StubComponent(data);
 			c->stub = stub;
 			return c;
 		});
